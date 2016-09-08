f288fa03284ee0dffff0399ab2c72b459fd61c5e
kavitha957 <kavithasna14@gmail.com>
2016-09-08 15:24:05 +0530

    Add files via upload

---
+++ wordrep.java
@@ -0,0 +1,13 @@
+package Program;
+
+import java.util.Scanner;
+
+public class wordrep {
+public static void main(String[] args) {
+	String str="hello";
+	for(int i=0;i<str.length();i++)
+	{
+		System.out.println(str);
+	}
+}
+}
