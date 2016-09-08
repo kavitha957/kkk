def41e5c67cffb8b7b74de1e53afa87c6112c9de
kavitha957 <kavithasna14@gmail.com>
2016-09-08 15:46:05 +0530

    Add files via upload

---
+++ EvenNumBtIntervals.java
@@ -0,0 +1,17 @@
+package Program;
+
+import java.util.Scanner;
+
+public class EvenNumBtIntervals {
+public static void main(String[] args) {
+	
+	for(int i=0;i<100;i++)
+	{
+		if(i%2==0)
+		{
+			System.out.println(i);
+		}
+			
+	}
+}
+}
